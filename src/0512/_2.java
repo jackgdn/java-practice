ce46b4f8e55019a8c569d9c0d9d423955ae5af32
jackgdn <li_zhong_yao@foxmail.com>
2025-05-12 16:40:16 +0800

    0512 commit

---
+++ src/0512/_2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class _2 {
+    public static String[] arr = { "星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日" };
+
+    public static void main(String args[]) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int d = sc.nextInt();
+            try {
+                System.out.println(arr[d - 1]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("输入错误");
+            }
+        }
+    }
+}
